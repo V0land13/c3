6d893ec4f6b9a02ded42e78299d3563fbe60f549
V0land13 <50770856+V0land13@users.noreply.github.com>
2019-09-20 13:06:04 +0300

    updete script2.js
    add a coments

--- script-2.js
+++ script-2.js
@@ -1,22 +1,21 @@
-const all_checkbox_num1 = $('.checkbox_num1')
-console.log($('.checkbox_num1'))
-var check_limit = 3;
+const all_checkbox_num1 = $('.checkbox_num1') //выбираем все чекбоксы
+var check_limit = 3; //Задаю количество выборов
+
+// Функция по загрузке проверяет есть ли нужные куки, если есть ставит нужные галочки
 document.addEventListener("DOMContentLoaded", function() {
     if (document.cookie.includes("choise"))
     {
-        var all_cookie_arr = document.cookie.split(";")
+        var all_cookie_arr = document.cookie.split(";") //масив всех куки
+        //Иасив нужных куки
         var cookie_arr = all_cookie_arr.filter(cookie => cookie.includes("choise"));
-        console.log('yjdsq vfcbd: ' + cookie_arr)
-        console.log(cookie_arr, cookie_arr.length)
+        // перебераю все куки и расставляю чекбоксы
         for (var jj = 0; jj < cookie_arr.length; jj++) {
             var cookie_val = cookie_arr[jj].split("=")[1]
-            //console.log($(`[value=${cookie_val}]`))
-            //$(`[value=${cookie_val}]`)[0].setAttribute("checked", "true");
             document.getElementsByClassName(cookie_val)[0].setAttribute("checked", "true");
         }
+        //Если куки 3 то деактивирую все чекбоксы
         if (cookie_arr.length == 3)
         {
-            console.log('delayu ecli est 3 cookie')
             for (var i = 0; i < all_checkbox_num1.length; i++) {
                 console.log(all_checkbox_num1[i])
                 all_checkbox_num1[i].setAttribute("disabled", "disabled");
@@ -37,35 +36,35 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-
-
+// в данном блоке отрабатываются выборы 1го и 2го чекбоксов
+// а также утановки куки
 $('input.checkbox_num1').on('change', function(evt) {
     var num_of_checked = $(this).siblings(':checked').length
-    console.log(num_of_checked)
     if (this.checked) { 
         var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=2678400‬";
         document.cookie = cookie_str;
     }
+    else {
+        var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=-1";
+        document.cookie = cookie_str;
+    }
     if($(this).siblings(':checked').length >= check_limit) {
         this.checked = false;
-        console.log('3')
     }
 });
-
+// действия при выборе 3го чекбокса
 $('input.checkbox_num1').on('input', function(evtt) {
     if($(this).siblings(':checked').length == (check_limit-1)) {
         var num_of_checked = $(this).siblings(':checked').length
         var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=2678400‬";
         document.cookie = cookie_str;
         for (var i = 0; i < all_checkbox_num1.length; i++) {
-            console.log(all_checkbox_num1[i])
             all_checkbox_num1[i].setAttribute("disabled", "disabled");
         }
     }
 });
-console.log(all_checkbox_num1)
 
+// кнопка сброса куки этой страницы и перезагрузки страницы
 reset_c.onclick = () => {
     document.cookie = "choise0=none; max-age=-1"
     document.cookie = "choise1=none; max-age=-1"
