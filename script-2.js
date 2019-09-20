17a89bcc04a6e12fab52cec8e1bff9d9142bc8ca
V0land13 <50770856+V0land13@users.noreply.github.com>
2019-09-20 15:09:15 +0300

    Update script2
    chenge cookie name and hope it is final

--- script-2.js
+++ script-2.js
@@ -1,13 +1,16 @@
 const all_checkbox_num1 = $('.checkbox_num1') //выбираем все чекбоксы
 var check_limit = 3; //Задаю количество выборов
-
+var all_checkbox_values = get_class_values ('.checkbox_num1');
+console.log(all_checkbox_values)
 // Функция по загрузке проверяет есть ли нужные куки, если есть ставит нужные галочки
 document.addEventListener("DOMContentLoaded", function() {
-    if (document.cookie.includes("choise"))
+    if (check_str_with_arr(document.cookie, all_checkbox_values))
     {
+        console.log('нужные куки е')
         var all_cookie_arr = document.cookie.split(";") //масив всех куки
-        //Иасив нужных куки
-        var cookie_arr = all_cookie_arr.filter(cookie => cookie.includes("choise"));
+        //Масив нужных куки
+        var cookie_arr = all_cookie_arr.filter(fnc => all_checkbox_values.includes(fnc.split("=")[1]));
+        console.log(cookie_arr)
         // перебераю все куки и расставляю чекбоксы
         for (var jj = 0; jj < cookie_arr.length; jj++) {
             var cookie_val = cookie_arr[jj].split("=")[1]
@@ -41,11 +44,11 @@ document.addEventListener("DOMContentLoaded", function() {
 $('input.checkbox_num1').on('change', function(evt) {
     var num_of_checked = $(this).siblings(':checked').length
     if (this.checked) { 
-        var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=2678400‬";
+        var cookie_str = this.value + '=' + this.value + '; max-age=2678400‬';
         document.cookie = cookie_str;
     }
     else {
-        var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=-1";
+        var cookie_str = this.value + '=' + this.value + '; max-age=-1';
         document.cookie = cookie_str;
     }
     if($(this).siblings(':checked').length >= check_limit) {
@@ -56,7 +59,7 @@ $('input.checkbox_num1').on('change', function(evt) {
 $('input.checkbox_num1').on('input', function(evtt) {
     if($(this).siblings(':checked').length == (check_limit-1)) {
         var num_of_checked = $(this).siblings(':checked').length
-        var cookie_str = 'choise'+ num_of_checked + '=' + this.value + "; max-age=2678400‬";
+        var cookie_str = this.value + '=' + this.value + '; max-age=2678400‬';
         document.cookie = cookie_str;
         for (var i = 0; i < all_checkbox_num1.length; i++) {
             all_checkbox_num1[i].setAttribute("disabled", "disabled");
@@ -66,8 +69,32 @@ $('input.checkbox_num1').on('input', function(evtt) {
 
 // кнопка сброса куки этой страницы и перезагрузки страницы
 reset_c.onclick = () => {
-    document.cookie = "choise0=none; max-age=-1"
-    document.cookie = "choise1=none; max-age=-1"
-    document.cookie = "choise2=none; max-age=-1"
+    all_checkbox_values.forEach( function(element, index) {
+        // statements
+        document.cookie = `${element}=${element}; max-age=-1`
+    });
+    //document.cookie = "choise0=none; max-age=-1"
     window.location.reload();
+}
+
+function check_str_with_arr(str, arr) {
+    // принимает строку и масив и проверякт есть ли элементы масива
+    // в строке, возвращает try если есть хотябы 1 
+    var result_cswa = false;
+    for (var i = 0; i < arr.length; i++) {
+            if (str.includes(arr[i])) result_cswa = true;
+    }
+    return result_cswa
+}
+
+
+
+function get_class_values (cls_str) {
+    // построение мавива значений атрибута value объекта класса
+    var cls_obj = $(cls_str);
+    var gcv = [];
+    for (var i = 0; i < cls_obj.length; i++){
+        gcv[i] = cls_obj[i].value;
+    }
+    return gcv
 }
